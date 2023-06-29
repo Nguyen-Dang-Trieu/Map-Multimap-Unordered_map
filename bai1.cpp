9e21c421b4b6d4a1784d4efc1449550be06aaa1c
Nguyen-Dang-Trieu <121329547+Nguyen-Dang-Trieu@users.noreply.github.com>
2023-06-29 22:50:43 +0700

    Create bai1.cpp

---
+++ bai1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main(void)
+{
+  map<string, int> _map;
+  //Insert some values into the map
+  _map["one"] = 1;
+  _map["two"] = 2;
+  _map["three"] = 3;
+
+  //Get an iterator pointing to the first element in the map
+  map<string, int>::iterator itr = _map.begin();
+
+  //Iterate through the map and print the elements
+  while(itr != _map.end())
+  {
+    cout<<"key: " << itr->first << ", value: " << itr->second << endl;
+    ++itr;
+  }
+  return 0;
+}
